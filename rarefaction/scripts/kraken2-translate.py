d94df40dd817997bd76a5f21edaedde633ff0921
LLansing <colelansing@gmail.com>
2020-10-08 13:37:47 -0700

    add file checking

--- rarefaction/scripts/kraken2-translate.py
+++ rarefaction/scripts/kraken2-translate.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+
+import os
+
 pID = 'TRANSLATION STEP:'
 
 """
@@ -7,17 +10,26 @@ MAIN
 # =============================================================================
 """
 def main():
+    # retrieve the name of the inspection file from snakemake
     db_inspection = snakemake.input[0]
     print(pID, db_inspection)
 
-    input2 = snakemake.input[1]
-    print(pID, input2)
+    # retrieve the paths of filtered files from snakemake
+    infile_paths = snakemake.input[1:]
+    print(pID, len(infile_paths))
+
+    # check file existence
+    print(pID, 'current directory:', os.getcwd)
+    print(pID, 'does {} exist'.format(db_inspection), os.path.isfile(db_inspection))
+    
 
 
 """
 # =============================================================================
 # =============================================================================
 """
+
+
 if __name__ == '__main__':
 
     main()
